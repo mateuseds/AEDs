182b1c3416f98780326ec4f8db75edfff4d0e515
Mateus Souza <80061497+mateuseds@users.noreply.github.com>
2023-03-29 13:48:00 -0300

    att

--- lista6aeds.c
+++ lista6aeds.c
@@ -1,271 +1,310 @@
-Nome: Mateus Estevão de Souza
-Matrícula: 771697
-
 #include <stdio.h>
 
+int ex01();
+int ex02();
+int ex03();
+int ex04();
+int ex05();
+int ex06();
+int ex07();
+int ex08();
+int ex09();
+int ex10();
+int ex11();
+int ex12();
+int ex13();
 int main() {
-    int opcao;
+    int op;
+    printf("Aluno: Mateus Estevao de Souza / Matricula: 771697\n");
+    printf("Escolha um exercicio:\n");
+    printf("1. Soma de n números\n");
+    printf("2. Sequência de Fibonacci com n elementos\n");
+    printf("3. Sequência de Fibonacci até limite l\n");
+    printf("4. Múltiplos de 5\n");
+    printf("5. Digitação de idade\n");
+    printf("6. Enésimo termo da sequência de Fibonacci\n");
+    printf("7. Sequência de n números ímpares\n");
+    printf("8. Números que são múltiplos de 3 e/ou 5 e/ou 7 entre A e B\n");
+    printf("9. Números que são múltiplos de 3 e/ou 5 e/ou 7 entre A e B e soma dos valores\n");
+    printf("10. Soma de valores digitados\n");
+    printf("11. Múltiplos de 5 com for e do-while\n");
+    printf("12. Digitação de idades com for e do-while\n");
+    printf("13. Soma de valores digitados com for e while\n");
+    printf("0. Sair\n");
 
-    do {
-        printf("Escolha uma opção:\n");
-        printf("1. Soma de n números\n");
-        printf("2. Sequência de Fibonacci com n elementos\n");
-        printf("3. Sequência de Fibonacci até limite l\n");
-        printf("4. Múltiplos de 5\n");
-        printf("5. Digitação de idade\n");
-        printf("6. Enésimo termo da sequência de Fibonacci\n");
-        printf("7. Sequência de n números ímpares\n");
-        printf("8. Números que são múltiplos de 3 e/ou 5 e/ou 7 entre A e B\n");
-        printf("9. Números que são múltiplos de 3 e/ou 5 e/ou 7 entre A e B e soma dos valores\n");
-        printf("10. Soma de valores digitados\n");
-        printf("11. Múltiplos de 5 com for e do-while\n");
-        printf("12. Digitação de idades com for e while\n");
-        printf("0. Sair\n");
+    scanf("%d", &op);
 
-        scanf("%d", &opcao);
-
-        switch (opcao) {
-            case 1:
+    switch (op) {
+        case 1:
+            ex01();
+            break;
+        case 2:
+            ex02();
+            break;
+        case 3:
+            ex03();
+            break;
+        case 4:
+            ex04();
+            break;
+        case 5:
+            ex05();
+            break;
+        case 6:
+            ex06();
+            break;
+        case 7:
+            ex07();
+            break;
+        case 8:
+            ex08();
+            break;
+        case 9:
+            ex09();
+            break;
+        case 10:
+            ex10();
+            break;
+        case 11:
+            ex11();
+            break;
+        case 12:
+            ex12();
+            break;
+        case 13:
+            ex13();
+            break;
+        default:
+            printf("Opcao invalida\n");
+            return 0;
+    }
+}
+            int ex01(){
                 int n, soma = 0, numero;
-    printf("Digite a quantidade de números a serem somados: ");
-    scanf("%d", &n);
-        for(int i=1; i<=n; i++) {
-            printf("Digite o %dº número: ", i);
-            scanf("%d", &numero);
-            soma += numero;
-        }
+                printf("Digite a quantidade de números a serem somados: ");
+                scanf("%d", &n);
+                for(int i=1; i<=n; i++) {
+                    printf("Digite o %dº número: ", i);
+                    scanf("%d", &numero);
+                    soma += numero;
+                }
 
-        printf("A soma dos números é: %d\n", soma);
-                break;
-
-            case 2:
-                 int n, primeiro = 0, segundo = 1, proximo;
-        printf("Digite o número de elementos da série de Fibonacci: ");
-        scanf("%d", &n);
-
-        printf("Série de Fibonacci: ");
-
-        for(int i=0; i<n; i++) {
-            if(i <= 1) {
-                proximo = i;
-            } else {
-                proximo = primeiro + segundo;
-                primeiro = segundo;
-                segundo = proximo;
+                printf("A soma dos números é: %d\n", soma);
+                return 0;
             }
-            printf("%d ", proximo);
-        }
-        printf("\n");
-                break;
 
-            case 3:
-                // lógica para sequência de Fibonacci até limite l
-                break;
+            int ex02(){
+                int n, primeiro = 0, segundo = 1, proximo;
+                printf("Digite o número de elementos da série de Fibonacci: ");
+                scanf("%d", &n);
 
-            case 4:
-                // lógica para múltiplos de 5
-                break;
+                printf("Série de Fibonacci: ");
 
-            case 5:
-                // lógica para digitação de idade
-                break;
-
-            case 6:
-                // lógica para enésimo termo da sequência de Fibonacci
-                break;
-
-            case 7:
-                // lógica para sequência de n números ímpares
-                break;
-
-            case 8:
-                // lógica para números que são múltiplos de 3 e/ou 5 e/ou 7 entre A e B
-                break;
-
-            case 9:
-                // lógica para números que são múltiplos de 3 e/ou 5 e/ou 7 entre A e B e soma dos valores
-                break;
-
-            case 10:
-                // lógica para soma de valores digitados
-                break;
-
-            case 11:
-                // lógica para múltiplos de 5 com for e do-while
-                break;
-
-            case 12:
-                // lógica para digitação de idades com for e while
-                break;
-
-            case 0:
-                printf("Saindo...\n");
-                break;
-
-            default:
-                printf("Opção inválida!\n");
-        }
-    } while (opcao != 0);
-
-    return 0;
-}
-
-
-
-
-    case 1: {
-    
-        break;
-    }
-    case 2: {
-       
-        break;
-    }
-    case 3: {
-        int l, primeiro = 0, segundo = 1, proximo = 0;
-        printf("Digite o limite da série de Fibonacci: ");
-        scanf("%d", &l);
-
-        printf("Série de Fibonacci até %d: ", l);
-
-        while(proximo < l) {
-            if(proximo <= 1) {
-                proximo = segundo;
-            } else {
-                proximo = primeiro + segundo;
-                primeiro = segundo;
-                segundo = proximo;
+                for(int i=0; i<n; i++) {
+                    if(i <= 1) {
+                        proximo = i;
+                    } else {
+                        proximo = primeiro + segundo;
+                        primeiro = segundo;
+                        segundo = proximo;
+                    }
+                    printf("%d ", proximo);
+                }
+                printf("\n");
+                 return 0;
             }
-            if(proximo < l) {
-                printf("%d ", proximo);
+
+            int ex03(){
+                int l, primeiro = 0, segundo = 1, proximo = 0;
+                printf("Digite o limite da série de Fibonacci: ");
+                scanf("%d", &l);
+
+                printf("Série de Fibonacci até %d: ", l);
+
+                while(proximo < l) {
+                    if(proximo <= 1) {
+                        proximo = segundo;
+                    } else {
+                        proximo = primeiro + segundo;
+                        primeiro = segundo;
+                        segundo = proximo;
+                    }
+                    if(proximo < l) {
+                        printf("%d ", proximo);
+                    }
+                }
+                 return 0;
             }
-        }
-        printf("\n");
-        break;
-    }
-    case 4: {
-        int i = 1;
 
-        while(1) {
-            if(i % 5 == 0) {
-                printf("%d ", i);
+            int ex04(){
+                int i = 1;
+                while(1) {
+                    if(i % 5 == 0) {
+                        printf("%d ", i);
+                    }
+                    i++;
+                }
+                 return 0;
             }
-            i++;
-            if (i > 100) {
-                break;
+
+            int ex05(){
+                int idade = 0;
+
+                while(idade <= 0 || idade > 140) {
+                    printf("Digite sua idade: ");
+                    scanf("%d", &idade);
+                    if(idade <= 0 || idade > 140) {
+                        printf("Idade inválida. Tente novamente.\n");
+                    }
+                }
+
+                printf("Sua idade é: %d\n", idade);
+                 return 0;
             }
-        }
-        printf("\n");
-        break;
-    }
-    case 5: {
-    int idade;
-    
-    do {
-        printf("Digite uma idade valida (entre 0 e 140 anos): ");
-        scanf("%d", &idade);
-    } while (idade < 0 || idade > 140);
-    
-    printf("A idade digitada foi: %d\n", idade);
 
-  
-// 6 - Programa para calcular o N-ésimo termo da sequência de Fibonacci:
+            int ex06(){
+                int n, primeiro = 1, segundo = 1, proximo;
+                printf("Digite o valor de N: ");
+                scanf("%d", &n);
 
-int main() {
-  int n, anterior = 1, atual = 1, proximo;
-  
-  printf("Digite o valor de N: ");
-  scanf("%d", &n);
-  
-  for (int i = 3; i <= n; i++) {
-    proximo = anterior + atual;
-    anterior = atual;
-    atual = proximo;
-  }
-  
-  printf("O %do termo da sequencia de Fibonacci e: %d\n", n, atual);
-  
-  return 0;
-}
+                for(int i=3; i<=n; i++) {
+                    proximo = primeiro + segundo;
+                    primeiro = segundo;
+                    segundo = proximo;
+                }
 
-// 7 - Programa para mostrar na tela os n primeiros elementos da sequência 1, 3, 5, 7, 9,... n:
+                printf("O %dº termo da sequência de Fibonacci é: %d\n", n, segundo);
+                 return 0;
+            }
 
-int main() {
-  int n;
-  
-  printf("Digite o valor de N: ");
-  scanf("%d", &n);
-  
-  for (int i = 1; i <= n; i++) {
-    printf("%d ", 2 * i - 1);
-  }
-  
-  printf("\n");
-  
-  return 0;
-}
+            int ex07(){
+                int n;
+                printf("Digite o número de elementos da sequência: ");
+                scanf("%d", &n);
 
-// 8 - Programa para mostrar na tela os n primeiros elementos da sequência do exercício anterior que forem divisíveis por três e múltiplos de cinco ou divisíveis por sete:
+                printf("Sequência: ");
+                for(int i=1; i<=n; i++) {
+                    printf("%d ", 2*i-1);
+                }
+                 return 0;
+            }
 
-int main() {
-  int n;
-  
-  printf("Digite o valor de N: ");
-  scanf("%d", &n);
-  
-  for (int i = 1; i <= n; i++) {
-    int num = 2 * i - 1;
-    if ((num % 3 == 0 && num % 5 == 0) || num % 7 == 0) {
-      printf("%d ", num);
-    }
-  }
-  
-  printf("\n");
-  
-  return 0;
-}
+            int ex08(){
+                int n, contador = 0, num = 1;
+
+                printf("Digite o valor de n: ");
+                scanf("%d", &n);
+
+                while (contador < n) {
+                    if ((num % 3 == 0 && num % 5 == 0) || num % 7 == 0) {
+                    printf("%d ", num);
+                    contador++;
+                    }
+                    num++;
+                }
+                 return 0;
+            }
+
+            int ex09(){
+                int n, contador = 0, num = 1, a, b, soma = 0;
+
+                printf("Digite o valor de n: ");
+                scanf("%d", &n);
+
+                printf("Digite o valor de A: ");
+                scanf("%d", &a);
+
+                printf("Digite o valor de B: ");
+                scanf("%d", &b);
+
+                while (contador < n) {
+                    if (num > a && num < b && ((num % 3 == 0 && num % 5 == 0) || num % 7 == 0))
+                        { printf("%d ", num); soma += num; contador++;
+                    }
+                    num++;
+                }
+
+                printf("\nSoma dos valores: %d", soma);
+                 return 0;
+            }
+
+            int ex10(){
+                int valor, soma = 0;
+
+                do {
+                    printf("Digite um valor (ou 0 para encerrar): ");
+                    scanf("%d", &valor);
+                    soma += valor;
+                } while (valor != 0);
+
+                printf("A soma dos valores digitados é: %d", soma);
+                 return 0;
+            }
+
+            int ex11(){
+            int num;
+            for (num = 0; ; num++) { // laço infinito
+                if (num % 5 == 0) {
+                    printf("%d\n", num);
+                }
+            }
+
+            --------
+
+            int num = 0;
+            do {
+                if (num % 5 == 0) {
+                    printf("%d\n", num);
+                }
+                num++;
+            } while (1); // laço infinito
+            return 0;
+            }
+
+            int ex12(){
+            int idade;
+            for (;;) { // laço infinito
+                printf("Digite uma idade: ");
+                scanf("%d", &idade);
+                if (idade > 0) {
+                    printf("Idade digitada: %d\n", idade);
+                }
+            }
+
+            --------
+
+            int idade;
+            while (1) { // laço infinito
+                printf("Digite uma idade: ");
+                scanf("%d", &idade);
+                if (idade > 0) {
+                    printf("Idade digitada: %d\n", idade);
+                }
+            }
+            return 0;
+            }
+
+            int ex13(){
+            int num, soma = 0;
+            for (;;) { // laço infinito
+                printf("Digite um valor (ou 0 para sair): ");
+                scanf("%d", &num);
+                soma += num;
+                if (num == 0) {
+                }
+            }
+            printf("Soma dos valores digitados: %d\n", soma);
 
 
-// 9 - Mostrar somente os elementos maiores que A e menores que B na sequência do exercício anterior:
-
-int main() {
-    int n, i, num, a, b;
-
-    printf("Digite o valor de n: ");
-    scanf("%d", &n);
-
-    printf("Digite o valor de A: ");
-    scanf("%d", &a);
-
-    printf("Digite o valor de B: ");
-    scanf("%d", &b);
-
-    printf("Os elementos maiores que A e menores que B são: ");
-
-    for (i = 1; i <= n; i++) {
-        num = (2 * i) - 1;
-
-        if ((num > a) && (num < b) && ((num % 3 == 0 && num % 5 == 0) || num % 7 == 0)) {
-            printf("%d ", num);
-        }
-    }
-
-    return 0;
-}
-
-// 10 - Calcular a soma de valores fornecidos pelo usuário:
-
-int main() {
-    int num, soma = 0;
-
-    do {
-        printf("Digite um número (0 para sair): ");
-        scanf("%d", &num);
-
-        soma += num;
-    } while (num != 0);
-
-    printf("A soma dos valores é %d\n", soma);
-
-    return 0;
-}
+            --------
+            int num, soma = 0;
+            while (1) { // laço infinito
+                printf("Digite um valor (ou 0 para sair): ");
+                scanf("%d", &num);
+                soma += num;
+                if (num == 0) {
+                }
+            }
+            printf("Soma dos valores digitados: %d\n", soma);
+            return 0;
+            }
